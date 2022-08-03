7179f50b34a2ba341ed6a831dc35d97a66b4f84b
aswinraj <aswinrajkailath@gmail.com>
2022-08-03 19:05:52 +0530

    Deployed DAO smart contract

---
+++ hardhat-setup/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x879eF5308a8290c402d4CF9248c26A9dD5Fc96A9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
